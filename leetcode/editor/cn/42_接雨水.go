a27b812df4e40cc585f0adbffeefc974a3d892e8
liuke <1250585541@qq.com>
2025-03-21 00:35:25 +0800

    feat: 单调栈

--- leetcode/editor/cn/42_接雨水.go
+++ leetcode/editor/cn/42_接雨水.go
@@ -46,9 +46,43 @@ func trap(height []int) int {
 	return water
 }
 
+func trap2(height []int) int {
+
+	stack := make([]int, 0, len(height))
+	// todo 递减单调栈
+	sum := 0
+	for i := 0; i < len(height); i++ {
+		if len(stack) == 0 {
+			stack = append(stack, i)
+		} else if height[stack[len(stack)-1]] > height[i] {
+			stack = append(stack, i)
+		} else if height[stack[len(stack)-1]] == height[i] {
+			stack = stack[:len(stack)-1]
+			stack = append(stack, i)
+		} else {
+			//todo
+			for len(stack) > 0 && height[stack[len(stack)-1]] < height[i] {
+				mid := height[stack[len(stack)-1]]
+				stack = stack[:len(stack)-1]
+				if len(stack) > 0 {
+					minH := height[stack[len(stack)-1]]
+					if height[i] < minH {
+						minH = height[i]
+					}
+					sum += (minH - mid) * (i - stack[len(stack)-1] - 1)
+				}
+			}
+			stack = append(stack, i)
+		}
+	}
+	return sum
+}
+
 //leetcode submit region end(Prohibit modification and deletion)
 
 func main() {
 	res := trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1})
+	res2 := trap2([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1})
 	fmt.Print(res)
+	fmt.Print(res2)
 }
